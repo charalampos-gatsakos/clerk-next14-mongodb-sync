d31ef30792ebf3c31f385fa32497e7e3a665af67
Charalampos Gatsakos <charalamposgatsakos@gmail.com>
2024-05-03 01:38:39 +0300

    mdlw fix

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,16 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
-
-// Make sure that the `/api/webhooks/(.*)` route is not protected here
-export default clerkMiddleware()
-
-export const config = {
-  matcher: [ '/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-};
+import {
+    clerkMiddleware,
+    createRouteMatcher
+  } from '@clerk/nextjs/server';
+  
+  const isProtectedRoute = createRouteMatcher([
+    '/',
+  ]);
+  
+  export default clerkMiddleware((auth, req) => {
+    if (isProtectedRoute(req)) auth().protect();
+  });
+  
+  export const config = {
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  };
