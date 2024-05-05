0565cd3d1ddc52befb8010cd4a9a5b593d57de4f
Charalampos Gatsakos <charalamposgatsakos@gmail.com>
2024-05-05 03:24:12 +0300

    db fic 9

--- lib/db.ts
+++ lib/db.ts
@@ -19,12 +19,12 @@ if (!cached) {
 export const connectToDatabase = async () => {
     console.log('enter')
     if (cached.conn) return cached.conn;
-    
+
     if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
-    
+
     cached.promise = cached.promise || mongoose.connect
-    (MONGODB_URL, { dbName: 'sydaxiai', bufferCommands: false })
-    
+        (MONGODB_URL, { dbName: 'arxigos', bufferCommands: false })
+
     cached.conn = await cached.promise;
 
     return cached.conn;
